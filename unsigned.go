b3380f74c04b68a3784b732e28f8defeac353d6a
Stuart Highley <stuart.highley@gmail.com>
2023-10-12 16:35:08 +0100

    Moved buckets to static

--- unsigned.go
+++ unsigned.go
@@ -4,23 +4,24 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-const base = 8 // 2^7 -> 128 | 2^8 -> 256
+const base = 8 // 2^8 -> 256
 const numBuckets = 1 << base
 const mask = numBuckets - 1
+
+var buckets [numBuckets]int
 
 // Radix Sort
 func Sort[T constraints.Unsigned](slice []T) []T {
 
-	// Allocate a copy of the slice, and the buckets
+	// Allocate copy of input slice
 	sorted := make([]T, len(slice))
-	buckets := make([]int, numBuckets)
 
 	// Loop until we reach the largest column
 	var largest T = 1
 	for column := 0; largest>>column > 0; column += base {
 
 		// Accumulate the appropriate bucket for each element
-		clear(buckets)
+		clear(buckets[:])
 		for _, e := range slice {
 			largest |= e
 			buckets[(int(e)>>column)&mask]++
@@ -31,7 +32,7 @@ func Sort[T constraints.Unsigned](slice []T) []T {
 			buckets[i] += buckets[i-1]
 		}
 
-		// Use buckets to fill 'semiSorted' array
+		// Use buckets to fill 'semi-sorted' array
 		for i := len(slice) - 1; i >= 0; i-- {
 			e := slice[i]
 			b := (int(e) >> column) & mask
@@ -39,7 +40,7 @@ func Sort[T constraints.Unsigned](slice []T) []T {
 			sorted[buckets[b]] = e
 		}
 
-		// Swap the slices over to init for next loop
+		// Swap slices over for next iteration
 		slice, sorted = sorted, slice
 	}
 
